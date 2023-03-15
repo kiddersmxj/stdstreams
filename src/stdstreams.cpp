27b198891a06ef71cdd61417fab2040e736f8726
kiddersmxj <maxkiddmay@gmail.com>
2023-03-15 10:55:38 +0000

    removed old functions in favour of child obj proc

--- src/stdstreams.cpp
+++ src/stdstreams.cpp
@@ -25,30 +25,3 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-FILE* LaunchChild(const char* Program) {
-    FILE *fp;
-
-    std::cout << "Running... " << Program << std::endl;
-
-    fp = popen(Program, "r");
-    if (fp == NULL) {
-        /* Handle error */;
-        std::cout << "popen error" << std::endl;
-    }
-    return fp;
-}
-
-std::string GetOutput(FILE* fp) {
-    char path[PATH_MAX];
-    if(fgets(path, PATH_MAX, fp) != NULL) {
-        printf("\033[0;35m");
-        printf("%s", path);
-        printf("\033[0m");
-
-        BreakPoint();
-
-        return path;
-    } else 
-        return "EXIT";
-}
-
