3eb172559414693a598bddf465f6a2f3adf7e8a9
kiddersmxj <maxkiddmay@gmail.com>
2023-03-17 10:18:18 +0000

    adjusted stdout to better fit changed requirments and standards

--- tests/stdout.sh
+++ tests/stdout.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-    printf "Mode=Neutral Speed=4 Status=Throttle but no drive selected\n"
+    printf 'Mode=Neutral, Speed=4, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status\n'
     sleep 0.5
 done
 
