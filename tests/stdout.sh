a59368100a725ae6299e132915d08623bcd24721
kiddersmxj <maxkiddmay@gmail.com>
2023-03-20 18:17:49 +0000

    added more varibles to parse

--- tests/stdout.sh
+++ tests/stdout.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-    printf 'Mode=Neutral, Speed=4, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status\n'
+    printf 'Mode=Neutral, Speed=4298, PotPosition=12, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status\n'
     sleep 0.5
 done
 
