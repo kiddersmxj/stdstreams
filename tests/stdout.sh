0a49a226a1df19384e5360cb33a63da0f8da6aec
kiddersmxj <maxkiddmay@gmail.com>
2023-03-21 18:17:31 +0000

    improved nature of tests by adding changing values

--- tests/stdout.sh
+++ tests/stdout.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+SLEEPTIME=0.1
+SPEED=40
+POTPOSITION=60
 while :
 do
-    printf 'Mode=Neutral, Speed=4298, PotPosition=12, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status\n'
-    sleep 0.5
+    i=0
+    while [ $i -le 10 ]; do
+        i=$(($i+1))
+        SPEED=$(($SPEED+$i))
+        POTPOSITION=$(($POTPOSITION-$i))
+        printf "Mode=Neutral, Speed=$SPEED, PotPosition=$POTPOSITION, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status\n"
+        sleep $SLEEPTIME
+    done
+    i=0
+    while [ $i -le 10 ]; do
+        i=$(($i+1))
+        SPEED=$(($SPEED-$i))
+        POTPOSITION=$(($POTPOSITION+$i))
+        printf "Mode=Neutral, Speed=$SPEED, PotPosition=$POTPOSITION, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status\n"
+        sleep $SLEEPTIME
+    done
 done
 
