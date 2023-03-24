fc8e5883619bcf59eba04e4d8648aacd0d8b9f33
kiddersmxj <maxkiddmay@gmail.com>
2023-03-24 18:37:54 +0000

    expanded no of statuses

--- tests/stdout.sh
+++ tests/stdout.sh
@@ -10,7 +10,7 @@ do
         i=$(($i+1))
         SPEED=$(($SPEED+$i))
         POTPOSITION=$(($POTPOSITION-$i))
-        printf "Mode=Neutral, Speed=$SPEED, PotPosition=$POTPOSITION, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status\n"
+        printf "Mode=Neutral, Speed=$SPEED, PotPosition=$POTPOSITION, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status, EMERGStatus=there is an emergency status\n"
         sleep $SLEEPTIME
     done
     i=0
