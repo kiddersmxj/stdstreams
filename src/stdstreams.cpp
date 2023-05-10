60d21ad49c62d4bb3ff402d9c00be3e352f5a63d
kiddersmxj <maxkiddmay@gmail.com>
2023-05-10 09:12:42 +0100

    reduced delay

--- src/stdstreams.cpp
+++ src/stdstreams.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
 #ifdef GDB
         k::BreakPoint();
 #else
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
 #endif
     }
 
