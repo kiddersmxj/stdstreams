57d95c209cbb2461b081104da4be20bb68c6c841
kiddersmxj <maxkiddmay@gmail.com>
2023-05-17 08:58:56 +0100

    added colour styles and updated no data mark

--- inc/config.hpp
+++ inc/config.hpp
@@ -3,11 +3,12 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
-#include <ftxui/screen/color.hpp>
+#include <ftxui/screen/color.hpp> // for Color
+#include <ftxui/dom/elements.hpp>  // for text, graph, operator|, separator, color, Element, vbox, flex, inverted, operator|=, Fit, hbox, size, border, GREATER_THAN, HEIGHT, Decorator
 
 const std::string ParentName = "stdstreams";
 const std::string Version = "0.0.0";
-const std::string NoDataMark = "NA";
+const std::string NoDataMarkInternal = "NA";
 const std::string UsageNotes = R"(
 usage: stdstreams [ -o/--options=value ] [ -p/--program=value ]
 make sure to give the full path of the program
@@ -24,5 +25,9 @@ the case.
 
 // Display
 const ftxui::Color GreyColours[4] = { ftxui::Color::Default, ftxui::Color::Grey70, ftxui::Color::Grey42, ftxui::Color::Grey23 };
+const ftxui::Decorator StrStyle = ftxui::color(ftxui::Color::Red);
+const ftxui::Decorator IntStyle = ftxui::color(ftxui::Color::Cyan);
+const ftxui::Decorator StatStyle = ftxui::color(ftxui::Color::Green);
+const ftxui::Decorator TextStyle = ftxui::color(ftxui::Color::Default);
 
 #endif
