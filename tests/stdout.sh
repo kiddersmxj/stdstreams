8d014a2cc4bd4c59b99675778d31950bff18e47f
kiddersmxj <maxkiddmay@gmail.com>
2023-05-17 17:49:09 +0100

    made pot position dependent on i^2 for additional tests

--- tests/stdout.sh
+++ tests/stdout.sh
@@ -10,7 +10,7 @@ do
     while [ $i -le 10 ]; do
         i=$(($i+1))
         SPEED=$(($SPEED+$i))
-        POTPOSITION=$(($POTPOSITION-$i))
+        POTPOSITION=$(($POTPOSITION-$(($i*$i))))
         THROTTLE=$(($THROTTLE+i/2))
         printf "Mode=Neutral, Speed=$SPEED, Potentiometer Position=$POTPOSITION, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status, EMERGStatus=there is an emergency status, Throttle=$THROTTLE\n"
         sleep $SLEEPTIME
@@ -19,7 +19,7 @@ do
     while [ $i -le 10 ]; do
         i=$(($i+1))
         SPEED=$(($SPEED-$i))
-        POTPOSITION=$(($POTPOSITION+$i))
+        POTPOSITION=$(($POTPOSITION+$(($i*$i))))
         THROTTLE=$(($THROTTLE-i/2))
         printf "Mode=Neutral, Speed=$SPEED, Potentiometer Position=$POTPOSITION, Status=Throttle but no drive selected, Status2=Fat error code pls handle, Emerg=, longer status=long status, Throttle=$THROTTLE\n"
         sleep $SLEEPTIME
