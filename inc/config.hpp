0c74e5094af56f5b0c9c458ebd77f53ad80c50d2
kiddersmxj <maxkiddmay@gmail.com>
2023-05-17 18:16:22 +0100

    code organisation and comments

--- inc/config.hpp
+++ inc/config.hpp
@@ -1,14 +1,10 @@
 #ifndef KCONFIG
 #define KCONFIG
-#include <cstdint>
-#include <iostream>
-#include <vector>
 #include <ftxui/screen/color.hpp> // for Color
 #include <ftxui/dom/elements.hpp>  // for text, graph, operator|, separator, color, Element, vbox, flex, inverted, operator|=, Fit, hbox, size, border, GREATER_THAN, HEIGHT, Decorator
 
 const std::string ParentName = "stdstreams";
 const std::string Version = "0.0.0";
-const std::string NoDataMarkInternal = "NA";
 const std::string UsageNotes = R"(
 usage: stdstreams [ -o/--options=value ] [ -p/--program=value ]
 Make sure to give the full path of the program
@@ -22,7 +18,7 @@ Program can be utilised to parse any program that can or does
 write to stdout. 
 
 Format specified as such:
-Mode=Neutral, Speed=41, Potentiometer Position=29, Status=Throttle 
+Mode=Neutral, Speed=41, Potentiometer Position=-29, Status=Throttle
 but no drive selected\n,
 
 The use of \n at the end of each line or equivilent endl character
@@ -37,17 +33,20 @@ the case.
 )";
 
 // Display
+// Colours
 const ftxui::Color GreyColours[4] = { ftxui::Color::Default, ftxui::Color::Grey70, ftxui::Color::Grey42, ftxui::Color::Grey23 };
 const ftxui::Decorator StrStyle = ftxui::color(ftxui::Color::Red);
 const ftxui::Decorator IntStyle = ftxui::color(ftxui::Color::Cyan);
 const ftxui::Decorator StatStyle = ftxui::color(ftxui::Color::Green);
 const ftxui::Decorator TextStyle = ftxui::color(ftxui::Color::Default);
-
+// Titles
 const std::string StatusTitle = "Status";
 const std::string MinTitle = "min: ";
 const std::string MaxTitle = "max: ";
 const std::string AvgTitle = "avg: ";
 
+// Output
+const std::string NoDataMarkInternal = "NA";
 const int MaxValueStorage = 400;
 
 #endif
