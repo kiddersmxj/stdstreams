4103f5e6657d3bf3c4d2919a9d564cc95467644c
kiddersmxj <maxkiddmay@gmail.com>
2023-05-22 09:33:12 +0100

    added LICENSE

--- src/child.cpp
+++ src/child.cpp
@@ -1,3 +1,9 @@
+/* Copyright (c) 2023, Maxamilian Kidd-May */
+/* All rights reserved. */
+
+/* This source code is licensed under the MIT license found in the */
+/* LICENSE file in the root directory of this source tree. */ 
+
 #include "../inc/child.hpp"
 
 // Constructor to launch child
